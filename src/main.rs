15bb3368bb73023b62d570b89dd6d466ec81fbc7
Matteo Vincenti <Vimat1990@outlook.com>
2022-05-20 23:07:29 +0200

    feat: add base route based on actix-web 1.0.8

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+
 fn main() {
-    println!("Hello, world!");
+    let server = HttpServer::new(|| { 
+        App::new().route("/", web::get().to(get_index))
+    });
+    
+    println!("Serving on http://localhost:3000");
+    
+    server.bind("0.0.0.0:3000")
+        .expect("Error binding server to address")
+        .run()
+        .expect("Error running server");
+}
+
+fn get_index() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(r#"
+            <title>Hello</title>
+            <form action="/gcd" method="post">
+                <input type="text" name="n">
+                <input type="text" name="m">
+                <button type="submit">Compute GCD</button>
+            </form>
+        "#,)
 }
